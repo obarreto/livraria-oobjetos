a9f8c8dd99c6319ce14ed2dfd8f690005fed3611
obarreto <ozbarreto@outlook.com>
2026-01-19 11:41:14 -0300

    guardando referencia produtos

--- src/br/com/ozystem/livraria/produto/CarrinhoDeCompras.java
+++ src/br/com/ozystem/livraria/produto/CarrinhoDeCompras.java
@@ -3,12 +3,15 @@ package br.com.ozystem.livraria.produto;
 public class CarrinhoDeCompras {
 		
 	private double total;
+	private Produto[] produtos = new Produto[10];
+	private int contador = 0;
+	
 	public void adiciona(Produto produto) {
 		System.out.println("Adicionando: " + produto);
-		//livro.aplicaDescontoDe(0.16);
-		total += produto.getValor();
+		this.produtos[contador] = produto;
+		contador ++;
+		this.total += produto.getValor();
 	}
-
 	public double getTotal() {
 		return total;
 	}
